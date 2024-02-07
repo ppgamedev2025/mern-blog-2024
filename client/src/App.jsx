4140ec5be0a9b33909d61e3a0804dc161b969e5b
dosui-pat <pp.gamedev2025@gmail.com>
2024-02-07 14:26:54 +0700

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Dashboard from './pages/Dashboard'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+
+
+
 export default function App() {
   return (
-    <div className="text-3x text-red-500">app</div>
+    <BrowserRouter >
+      <Routes>
+        <Route path='/' element={<Home />} /> 
+        <Route path='/About' element={<About />} />
+        <Route path='/Projects' element={<Projects />} />
+        <Route path='/Dashboard' element={<Dashboard />} />
+        <Route path='/SignedIn' element={<SignIn />} />
+        <Route path='/SignUp' element={<SignUp />} />
+
+      </Routes>
+
+    </BrowserRouter>
   )
 }
