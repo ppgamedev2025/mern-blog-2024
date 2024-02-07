276b2a20fa65a03e5c01d86b2a1fc11ec85c7958
dosui-pat <pp.gamedev2025@gmail.com>
2024-02-07 15:44:13 +0700

    create the header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,14 @@ import Projects from './pages/Projects'
 import Dashboard from './pages/Dashboard'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import Header from './components/Header'
 
 
 
 export default function App() {
   return (
     <BrowserRouter >
+      <Header/>
       <Routes>
         <Route path='/' element={<Home />} /> 
         <Route path='/About' element={<About />} />
