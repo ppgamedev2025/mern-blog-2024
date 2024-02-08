1b64e09c756015b2735d16f701e1df9f26bccaa4
dosui-pat <pp.gamedev2025@gmail.com>
2024-02-08 15:43:41 +0700

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -16,9 +17,13 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server running on port 3000!");
 });
 
 //Create API routes
 app.use("/api/user", userRoutes);
+
+app.use("/api/auth", authRoutes);
