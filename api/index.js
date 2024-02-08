de3fb42b8489b869e03c6edbe862b7f08b44a6d6
dosui-pat <pp.gamedev2025@gmail.com>
2024-02-08 16:02:20 +0700

    add middleware and a function to handle error

--- api/index.js
+++ api/index.js
@@ -27,3 +27,15 @@ app.listen(3000, () => {
 app.use("/api/user", userRoutes);
 
 app.use("/api/auth", authRoutes);
+
+
+//Craete Middleware routes
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+      statusCode,
+      message,
+  })
+})
